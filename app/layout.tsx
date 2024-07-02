221557f1207ccf62ee8f116d923d1fa8da63672e
Santiago Millan <santiagomillanpardo@gmail.com>
2024-07-02 01:34:00 -0500

    Feature: Primeros pasos, falta revisar el header, ajustar colores y definir orden

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "../components/organisms/Header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      {/* <body className={inter.className}> */}
+      <body className="max-w-screen-xl items-center justify-between mx-auto">
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
