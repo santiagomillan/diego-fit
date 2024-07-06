3a2ee490be1acfea36d404c7b69ed7735cd5a438
Santiago Millan <santiagomillanpardo@gmail.com>
2024-07-06 02:58:13 -0500

    Featire: Ajuste img hero banner

--- components/organisms/HeroBanner/HeroBanner.jsx
+++ components/organisms/HeroBanner/HeroBanner.jsx
@@ -1,50 +1,59 @@
+// export const HeroBanner = () => {
+//     return (
+//         <div className="relative isolate overflow-hidden">
+//             <img src="https://images.pexels.com/photos/791763/pexels-photo-791763.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+//             alt="hero banner" 
+//             // className="-z-10 absolute inset-0 object-cover w-full h-full" 
+//             className="absolute inset-0 -z-10 h-full w-full stroke-white/100 [mask-image:radial-gradient(100%_200%_at_top_right,white,transparent)]"
+//             />
+//         <div className="mt-[-50px] flex h-screen items-center justify-center">
+//           <div className="max-w-full flex-shrink-0 px-4 text-center lg:mx-0 lg:max-w-3xl lg:pt-8">
+//             <h1 className="mt-10 text-5xl font-bold tracking-tight text-white">
+//                 Transforma tu 
+//               <span className="text-sky-500"> vida</span> con el
+//               <span className="text-sky-500"> Entrenamiento Personalizado</span>
+//             </h1>
+//             <p className="mt-6 text-lg leading-8 text-gray-300">Descubre cómo alcanzar tus metas con un plan de entrenamiento hecho a tu medida.</p>
+//             <div className="mt-5 flex items-center justify-center gap-x-6">
+//               <a href="/"
+//                 className="rounded-md bg-sky-500 px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-sky-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-400"
+//                 rel="noreferrer">Comienza Hoy →</a>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   };
+  
 export const HeroBanner = () => {
     return (
-        <div className="relative isolate overflow-hidden">
-            <img src="https://images.pexels.com/photos/791763/pexels-photo-791763.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
-            alt="hero banner" 
-            // className="-z-10 absolute inset-0 object-cover w-full h-full" 
-            className="absolute inset-0 -z-10 h-full w-full stroke-white/100 [mask-image:radial-gradient(100%_200%_at_top_right,white,transparent)]"
-            />
-
-        {/* <svg
-          className="absolute inset-0 -z-10 h-full w-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
-          aria-hidden="true">
-          <defs>
-            <pattern id="983e3e4c-de6d-4c3f-8d64-b9761d1534cc" width="200" height="200" x="100%" y="-1"
-              patternUnits="userSpaceOnUse">
-              <path d="M.5 200V.5H200" fill="none"></path>
-            </pattern>
-          </defs>
-          <svg x="50%" y="-1" className="overflow-visible fill-gray-800/20">
-            <path d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
-              strokeWidth="0"></path>
-          </svg>
-          <rect width="100%" height="100%" strokeWidth="0" fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)"></rect>
-        </svg>
-        <div
-          className="absolute left-[calc(50%-4rem)] top-10 -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]"
-          aria-hidden="true">
-          <div className="aspect-[1108/632] w-[69.25rem] bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-20"
-            style={{ clipPath: "polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)" }}>
-          </div>
-        </div> */}
-        <div className="mt-[-50px] flex h-screen items-center justify-center">
-          <div className="max-w-full flex-shrink-0 px-4 text-center lg:mx-0 lg:max-w-3xl lg:pt-8">
+      <div className="relative isolate overflow-hidden h-screen">
+        <img 
+          src="https://images.pexels.com/photos/791763/pexels-photo-791763.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+          alt="hero banner" 
+          className="absolute inset-0 -z-10 w-full h-full object-cover object-center [mask-image:radial-gradient(100%_200%_at_top_right,white,transparent)]"
+        />
+        <div className="flex h-full items-center justify-center">
+          <div className="max-w-full px-4 text-center lg:mx-0 lg:max-w-3xl lg:pt-8">
             <h1 className="mt-10 text-5xl font-bold tracking-tight text-white">
-                Transforma tu 
+              Transforma tu 
               <span className="text-sky-500"> vida</span> con el
               <span className="text-sky-500"> Entrenamiento Personalizado</span>
             </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-300">Descubre cómo alcanzar tus metas con un plan de entrenamiento hecho a tu medida.</p>
+            <p className="mt-6 text-lg leading-8 text-gray-300">
+              Descubre cómo alcanzar tus metas con un plan de entrenamiento hecho a tu medida.
+            </p>
             <div className="mt-5 flex items-center justify-center gap-x-6">
-              <a href="/"
+              <a 
+                href="/"
                 className="rounded-md bg-sky-500 px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-sky-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-400"
-                rel="noreferrer">Comienza Hoy →</a>
+                rel="noreferrer"
+              >
+                Comienza Hoy →
+              </a>
             </div>
           </div>
         </div>
       </div>
     );
   };
-  
